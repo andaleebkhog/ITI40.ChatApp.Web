d57c445e42aa16509402c4e946721dc667f9fda8
andaleeb <andaleebkhog@gmail.com>
2020-05-05 17:29:02 +0200

    Add project files.

---
+++ ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace ITI40.ChatApp.Web
+{
+    public class ChatHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}
